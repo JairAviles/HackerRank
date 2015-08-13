2825244475ccbdeee20558e84287ce16ba21cdbc
Jair Avilés <hi@jairaviles.mx>
2015-08-13 16:55:40 -0500

    add 04_Japanese_Cities_Name.sql file to Select folder

---
+++ SQL/Select/04_Japanese_Cities_Name.sql
@@ -0,0 +1,12 @@
+/*******************************************************************
+* 				º Japanese Cities' Name º					 	   *
+* 														     	   *
+* Author:    Jair Israel Avilés Eusebio						 	   *
+* Date:      07/27/2015									    	   *
+* Language:  Oracle										     	   *
+* Twitter:   @javiles87										 	   *
+* URL: https://www.hackerrank.com/challenges/japanese-cities-name  *
+* 															       *
+* ******************************************************************/
+
+SELECT Name FROM City WHERE CountryCode = 'JPN';
