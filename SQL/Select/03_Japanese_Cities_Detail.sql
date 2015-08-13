be60be4fffae0fe073f6073b23280126d6369f48
Jair Avilés <hi@jairaviles.mx>
2015-08-13 16:52:37 -0500

    add 03_Japanese_Cities_Detail.sql to Select folder

---
+++ SQL/Select/03_Japanese_Cities_Detail.sql
@@ -0,0 +1,12 @@
+/**********************************************************************
+* 				º Japanese Cities' Detail º			  				  *
+* 														     		  *
+* Author:    Jair Israel Avilés Eusebio						 		  *
+* Date:      07/27/2015									     		  *
+* Language:  Oracle										   			  *
+* Twitter:   @javiles87										 		  *
+* URL: https://www.hackerrank.com/challenges/japanese-cities-detail	  *
+* 															 		  *
+* *********************************************************************/
+
+SELECT * FROM City WHERE CountryCode = 'JPN';
