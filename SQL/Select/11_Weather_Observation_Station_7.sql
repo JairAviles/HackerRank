f210e83fcd2ab03d7fc2fb8db1c35b3c1515e775
Jair Avilés <hi@jairaviles.mx>
2015-08-13 17:06:13 -0500

    add 10_Weather_Observation_Station_6.sql to Select folder
    add 11_Weather_Observation_Station_7.sql to Select folder
    
    add 12_Weather_Observation_Station_8.sql to Select folder
    
    add 13_Weather_Observation_Station_9.sql to Select folder
    
    add 14_Weather_Observation_Station_10.sql to Select folder

---
+++ SQL/Select/11_Weather_Observation_Station_7.sql
@@ -0,0 +1,12 @@
+/****************************************************************************
+* 				º Weather Observation Station 7 º			 	   		    *
+* 														     	   		    *
+* Author:    Jair Israel Avilés Eusebio						 	   	        *
+* Date:      07/27/2015									    	   		    *
+* Language:  Oracle										     	   		    *
+* Twitter:   @javiles87										 	   		    *
+* URL: https://www.hackerrank.com/challenges/weather-observation-station-7  *
+* 															       		    *
+* ***************************************************************************/
+
+SELECT DISTINCT City FROM STATION WHERE SUBSTR(City, LENGTH(City),1) IN ('a','e','i','o','u','A','E','I','O','U') ORDER BY City;
