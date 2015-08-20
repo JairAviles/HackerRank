69e544d702d4f94f311a24bd09737d53aca919a2
Jair Avilés <hi@jairaviles.mx>
2015-08-19 19:02:52 -0500

    Add README.md file to Java folder
    Add README.md file to SQL folder

--- Java/Introduction/WelcomeToJava.java
+++ Java/Introduction/WelcomeToJava.java
@@ -1,3 +1,14 @@
+/***************************************************************************
+* 						º Welcome to Java! º				 	   		   *
+* 														     	   		   *
+* Author:    Jair Israel Avilés Eusebio						 	   	       *
+* Date:      05/30/2015									    	   		   *
+* Language:  Java										     	   		   *
+* Twitter:   @javiles87										 	   		   *
+* URL: https://www.hackerrank.com/challenges/welcome-to-java		       *
+* 															       		   *
+* **************************************************************************/
+
 public class WelcomeToJava {
 //public class Solution {
     public static void main(String [] args) {
