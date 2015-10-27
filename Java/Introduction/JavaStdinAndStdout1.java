001df12526a2745611aa5025479da942fced59bd
Jair Avilés <hi@jairaviles.mx>
2015-10-27 17:48:48 -0600

    [Add] Add .gitignore file
    [Update] Update WelcomeToJava.java file

---
+++ Java/Introduction/JavaStdinAndStdout1.java
@@ -0,0 +1,32 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/***************************************************************************
+* 						º Java Stdin and Stdout 1 º			 	   		   *
+* 														     	   		   *
+* Author:    Jair Israel Avilés Eusebio						 	   	       *
+* Date:      10/27/2015									    	   		   *
+* Language:  Java										     	   		   *
+* Twitter:   @javiles87										 	   		   *
+* URL: https://www.hackerrank.com/challenges/java-stdin-and-stdout-1       *
+* 															       		   *
+* **************************************************************************/
+
+public class JavaStdinAndStdout1 {
+	//public class Solution {
+
+	    public static void main(String[] args) {
+	      Scanner sc=new Scanner(System.in);
+	      int a = 0;
+
+	      while(sc.hasNextLine()) {
+	           try {
+	              a = sc.nextInt();
+
+	              System.out.println(a);
+	           } catch (InputMismatchException e) {
+	               e.printStackTrace();
+	           }
+	      }
+	    }
+}
--- Java/Introduction/WelcomeToJava.java
+++ Java/Introduction/WelcomeToJava.java
@@ -14,5 +14,5 @@ public class WelcomeToJava {
     public static void main(String [] args) {
         System.out.println("Hello World.");
         System.out.println("Hello Java.");
-    }    
+    }
 }
